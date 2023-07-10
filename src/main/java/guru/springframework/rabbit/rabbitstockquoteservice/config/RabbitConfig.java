129c9c516abdea7d1c2a38b172a6446f8c1ecba6
Omar Silva <abettermanep@gmail.com>
2023-07-10 17:36:23 -0600

    Adding receive message

--- src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/config/RabbitConfig.java
+++ src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/config/RabbitConfig.java
@@ -1,14 +1,14 @@
 package guru.springframework.rabbit.rabbitstockquoteservice.config;
 
 import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Delivery;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.rabbitmq.RabbitFlux;
-import reactor.rabbitmq.Sender;
-import reactor.rabbitmq.SenderOptions;
+import reactor.rabbitmq.*;
 
 import javax.annotation.PreDestroy;
 
@@ -33,4 +33,10 @@ public class RabbitConfig {
     Sender sender(Mono<Connection> mono) {
         return RabbitFlux.createSender(new SenderOptions().connectionMono(mono));
     }
+
+    @Bean
+    Receiver receiver(Mono<Connection> mono) {
+        return RabbitFlux.createReceiver(new ReceiverOptions().connectionMono(mono));
+    }
+
 }
--- src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/service/QuoteMessageSender.java
+++ src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/service/QuoteMessageSender.java
@@ -5,11 +5,16 @@ import guru.springframework.rabbit.rabbitstockquoteservice.config.RabbitConfig;
 import guru.springframework.rabbit.rabbitstockquoteservice.model.Quote;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.rabbitmq.OutboundMessage;
+import reactor.rabbitmq.OutboundMessageResult;
+import reactor.rabbitmq.QueueSpecification;
 import reactor.rabbitmq.Sender;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class QuoteMessageSender {
@@ -20,6 +25,18 @@ public class QuoteMessageSender {
     public Mono<Void> sendQuoteMessage(Quote quote){
         byte[] jsonBytes = objectMapper.writeValueAsBytes(quote);
 
-        return sender.send(Mono.just(new OutboundMessage("", RabbitConfig.QUEUE, jsonBytes)));
+        Flux<OutboundMessageResult> confirmations = sender.sendWithPublishConfirms(
+                Flux.just(new OutboundMessage("", RabbitConfig.QUEUE, jsonBytes)));
+
+        sender.declareQueue(QueueSpecification.queue(RabbitConfig.QUEUE))
+                .thenMany(confirmations)
+                .doOnError(e -> log.error("Send failed", e))
+                .subscribe(r -> {
+                    if (r.isAck()) {
+                        log.info("Message sent successfully {}", new String(r.getOutboundMessage().getBody()));
+                    }
+                });
+
+        return Mono.empty();
     }
 }
