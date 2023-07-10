a6a493036afd8669b8b230088b3a78b97abe4eb3
Omar Silva <abettermanep@gmail.com>
2023-07-10 15:34:48 -0600

    Adding message sender

--- src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/service/QuoteGeneratorServiceImpl.java
+++ src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/service/QuoteGeneratorServiceImpl.java
@@ -1,6 +1,5 @@
 package guru.springframework.rabbit.rabbitstockquoteservice.service;
 
-
 import guru.springframework.rabbit.rabbitstockquoteservice.model.Quote;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
